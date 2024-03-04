fb5848024468f9b58ad936f182a41d8cf3a26042
Thiago Alencar <talencar@gmail.com>
2024-03-04 15:52:02 -0300

    test: Fix dumped schema test

--- spec/graphql/tiny_insurer_graphql_schema_spec.rb
+++ spec/graphql/tiny_insurer_graphql_schema_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe TinyInsurerGraphqlSchema do
   it 'matches the dumped schema (rails graphql:schema:dump)' do
-    aggregate_failures do 
-      # expect(described_class.to_definition).to eq(File.read(Rails.root.join('schema.graphql')).rstrip)
-      expect(described_class.to_json).to eq(File.read(Rails.root.join('schema.json')).rstrip)
+    aggregate_failures do
+      expect(described_class.to_definition.rstrip).to eq(File.read(Rails.root.join('schema.graphql')).rstrip)
+      expect(described_class.to_json.rstrip).to eq(File.read(Rails.root.join('schema.json')).rstrip)
     end
   end
 end
