5eb596b86f4db0003a0399b5f5811f5a3d898f81
Thiago Alencar <talencar@gmail.com>
2024-04-16 16:00:22 -0300

    fix some security things

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,28 +1,7 @@
 # frozen_string_literal: true
 
 class WebhooksController < ApplicationController
-  # include Rails.application.routes.url_helpers
-
-  # server.rb
-  #
-  # Use this sample code to handle webhook events in your integration.
-  #
-  # 1) Paste this code into a new file (server.rb)
-  #
-  # 2) Install dependencies
-  #   gem install sinatra
-  #   gem install stripe
-  #
-  # 3) Run the server on http://localhost:4242
-  #   ruby server.rb
-
-  # The library needs to be configured with your account's secret key.
-  # Ensure the key is kept out of any version control system you might be using.
-  # Stripe.api_key = 'sk_test_...'
-
-  # This is your Stripe CLI webhook secret for testing your endpoint locally.
-  ENDPOINT_SECRET = 'whsec_IcXAvcyWkUliyCPncX6qddPsxNBIo7hy'
-  # set :port, 4242
+  ENDPOINT_SECRET = ENV.fetch('WEBHOOK_PAYMENT_SECRET', nil)
 
   def update # rubocop:disable all
     payload = request.body.read
